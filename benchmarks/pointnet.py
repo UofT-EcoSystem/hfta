8b7bbdab5ef7c68472dc398fc58a9cf7bdd2a4ae
eric-zheng <yuxuan.zheng@mail.utoronto.ca>
2021-01-11 22:23:20 -0500

    [PointNet] Add the missing input argument to fix the serial runner

--- benchmarks/pointnet.py
+++ benchmarks/pointnet.py
@@ -91,6 +91,7 @@ def main(args):
       enable_dcgm=args.enable_dcgm,
       epochs=args.epochs,
       iters_per_epoch=args.iters_per_epoch,
+      serial_runner_kwargs=args.serial_runner_kwargs,
       concurrent_runner_kwargs=args.concurrent_runner_kwargs,
       mps_runner_kwargs=args.mps_runner_kwargs,
       hfta_runner_kwargs=args.hfta_runner_kwargs,
