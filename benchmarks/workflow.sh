baaa67ee4a7f488dcd7d9da3efeaa0f21cef72ae
Shang Wang <samshang.wang@mail.utoronto.ca>
2021-01-25 04:32:14 -0500

    [HFHT] Adding hfht_workflow.sh.

--- benchmarks/workflow.sh
+++ benchmarks/workflow.sh
@@ -4,26 +4,7 @@ DEVICE=${1:-"cuda"}
 DEVICE_MODEL=${2:-"v100"}
 OUTDIR_ROOT=${3:-"benchmarks/"}
 
-_pointnet_warmup_data () {
-  local task=$1
-  if [ "${task}" == "cls" ]; then
-    python examples/pointnet/train_classification.py \
-      --epochs 1 \
-      --iters-per-epoch 1000 \
-      --dataset datasets/shapenetcore_partanno_segmentation_benchmark_v0/ \
-      --eval \
-      --warmup-data-loading
-  elif [ "${task}" == "seg" ]; then
-    python examples/pointnet/train_segmentation.py \
-      --epochs 1 \
-      --iters-per-epoch 1000 \
-      --dataset datasets/shapenetcore_partanno_segmentation_benchmark_v0/ \
-      --warmup-data-loading
-  else
-    echo "Unknown task: ${task} !"
-    return -1
-  fi
-}
+source benchmarks/utils.sh
 
 _workflow_pointnet () {
   local task=$1
