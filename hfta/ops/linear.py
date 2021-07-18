2a2a941d652bb9f45e1baa6ab4a56e0f9003860f
Shang Wang <samshang.wang@mail.utoronto.ca>
2021-07-18 02:37:55 -0400

    [Ops] Update linear.py to 1.9.

--- hfta/ops/linear.py
+++ hfta/ops/linear.py
@@ -2,36 +2,56 @@ import math
 
 import torch
 import torch.nn as nn
+from torch import Tensor
+from torch.nn import Module, Parameter, init
 
 
-class Linear(nn.Module):
+class Linear(Module):
   """ Input format: [B * F]
   B: Training array size (number of batches/jobs).
   *: any number of dimensions, such as Batch size.
   F: in_features.
   """
+  __constants__ = ['in_features', 'out_features', 'B']
+  in_features: int
+  out_features: int
+  weight: Tensor
+  B: int
 
-  def __init__(self, in_features, out_features, bias=True, B=1):
+  def __init__(
+      self,
+      in_features: int,
+      out_features: int,
+      bias: bool = True,
+      device=None,
+      dtype=None,
+      B=1,
+  ) -> None:
+    factory_kwargs = {'device': device, 'dtype': dtype}
     super(Linear, self).__init__()
     self.in_features = in_features
     self.out_features = out_features
     self.B = B
-    self.weight = nn.Parameter(torch.Tensor(B, in_features, out_features))
+    self.weight = Parameter(
+        torch.empty((B, in_features, out_features), **factory_kwargs))
     if bias:
-      self.bias = nn.Parameter(torch.Tensor(B, 1, out_features))
+      self.bias = Parameter(torch.empty((B, 1, out_features), **factory_kwargs))
     else:
       self.register_parameter('bias', None)
     self.reset_parameters()
 
-  def reset_parameters(self):
+  def reset_parameters(self) -> None:
+    # Setting a=sqrt(5) in kaiming_uniform is the same as initializing with
+    # uniform(-1/sqrt(in_features), 1/sqrt(in_features)). For details, see
+    # https://github.com/pytorch/pytorch/issues/57109
     for b in range(self.B):
-      nn.init.kaiming_uniform_(self.weight[b], a=math.sqrt(5), mode='fan_out')
+      init.kaiming_uniform_(self.weight[b], a=math.sqrt(5), mode='fan_out')
       if self.bias is not None:
-        _, fan_out = nn.init._calculate_fan_in_and_fan_out(self.weight[b])
-        bound = 1 / math.sqrt(fan_out)
-        nn.init.uniform_(self.bias[b], -bound, bound)
+        _, fan_out = init._calculate_fan_in_and_fan_out(self.weight[b])
+        bound = 1 / math.sqrt(fan_out) if fan_out > 0 else 0
+        init.uniform_(self.bias[b], -bound, bound)
 
-  def forward(self, x):
+  def forward(self, x: Tensor) -> Tensor:
     old_shape = list(x.size())
     x = x.view(old_shape[0], -1, old_shape[-1])
     if self.bias is None:
