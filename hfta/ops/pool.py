6489cda19fbc326737be3b186d4fe43fe2d17336
Shang Wang <samshang.wang@mail.utoronto.ca>
2021-07-18 22:48:02 -0400

    [Ops] Update pool.py to 1.9.

--- hfta/ops/pool.py
+++ hfta/ops/pool.py
@@ -2,10 +2,48 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch import Tensor
+from torch.nn import Module
+from torch.nn.common_types import (_size_any_t, _size_any_opt_t, _size_2_t,
+                                   _size_2_opt_t)
+from typing import Optional
 
 
-class MaxPool2d(nn.Module):
-  """ Input format: [N B C H W]
+class _MaxPoolNd(Module):
+  __constants__ = [
+      'kernel_size', 'stride', 'padding', 'dilation', 'return_indices',
+      'ceil_mode', 'B'
+  ]
+  return_indices: bool
+  ceil_mode: bool
+  B: int
+
+  def __init__(
+      self,
+      kernel_size: _size_any_t,
+      stride: Optional[_size_any_t] = None,
+      padding: _size_any_t = 0,
+      dilation: _size_any_t = 1,
+      return_indices: bool = False,
+      ceil_mode: bool = False,
+      B: int = 1,
+  ) -> None:
+    super(_MaxPoolNd, self).__init__()
+    self.kernel_size = kernel_size
+    self.stride = stride if (stride is not None) else kernel_size
+    self.padding = padding
+    self.dilation = dilation
+    self.return_indices = return_indices
+    self.ceil_mode = ceil_mode
+    self.B = B
+
+  def extra_repr(self) -> str:
+    return ('kernel_size={kernel_size}, stride={stride}, padding={padding} '
+            ', dilation={dilation}, ceil_mode={ceil_mode}, B={B}'.format(
+                **self.__dict__))
+
+
+class MaxPool2d(_MaxPoolNd):
+  r""" Input format: [N B C H W]
   B: Training array size (number of batches/jobs).
   N: Batch size.
   C: Number of channels.
@@ -13,42 +51,29 @@ class MaxPool2d(nn.Module):
   W: Width.
   """
 
-  def __init__(
-      self,
-      kernel_size,
-      stride=None,
-      padding=0,
-      dilation=1,
-      return_indices=False,
-      ceil_mode=False,
-      B=1,
-  ):
-    super(MaxPool2d, self).__init__()
-    self.pool = nn.MaxPool2d(
-        kernel_size,
-        stride=stride,
-        padding=padding,
-        dilation=dilation,
-        return_indices=return_indices,
-        ceil_mode=ceil_mode,
-    )
+  kernel_size: _size_2_t
+  stride: _size_2_t
+  padding: _size_2_t
+  dilation: _size_2_t
 
-  def forward(self, x):
-    N, B, C, H, W = x.size()
-    res = self.pool(x.view(N * B, C, H, W))
-    if self.pool.return_indices:
+  def forward(self, input: Tensor) -> Tensor:
+    N, B, C, H, W = input.size()
+    res = F.max_pool2d(input.view(N * B, C, H, W), self.kernel_size,
+                       self.stride, self.padding, self.dilation, self.ceil_mode,
+                       self.return_indices)
+    if self.return_indices:
       y, indices = res
     else:
       y = res
     NxB, C, H, W = y.size()
     y = y.view(N, B, C, H, W)
-    return (y, indices.view(N, B, C, H, W)) if self.pool.return_indices else y
+    return (y, indices.view(N, B, C, H, W)) if self.return_indices else y
 
 
-class _AdaptiveAvgPoolNd(nn.Module):
-  __constants__ = ['output_size']
+class _AdaptiveAvgPoolNd(Module):
+  __constants__ = ['output_size', 'B']
 
-  def __init__(self, output_size, B=1):
+  def __init__(self, output_size: _size_any_opt_t, B: int = 1) -> None:
     super(_AdaptiveAvgPoolNd, self).__init__()
     self.output_size = output_size
     self.B = B
@@ -58,13 +83,15 @@ class _AdaptiveAvgPoolNd(nn.Module):
 
 
 class AdaptiveAvgPool2d(_AdaptiveAvgPoolNd):
-  """ Input format: [N B C H W]
+  r""" Input format: [N B C H W]
   B: Training array size (number of batches/jobs).
   N: Batch size.
   C: Number of channels.
   H: Height.
   W: Width.
   """
+
+  output_size: _size_2_opt_t
 
   def forward(self, input: Tensor) -> Tensor:
     N, B, C, H, W = input.size()
