1c9358d09da6bba5e5c4a759ad37d82fba42b874
Xin Li <you@example.com>
2022-03-27 16:44:46 -0400

    add supprot for torch.nn.AvgPool2D

--- hfta/ops/pooling.py
+++ hfta/ops/pooling.py
@@ -96,3 +96,85 @@ class AdaptiveAvgPool2d(_AdaptiveAvgPoolNd):
     output = F.adaptive_avg_pool2d(input.view(N * B, C, H, W), self.output_size)
     NxB, C, H, W = output.size()
     return output.view(N, B, C, H, W)
+
+
+class _AvgPoolNd(Module):
+    __constants__ = ['kernel_size', 'stride', 'padding', 'ceil_mode', 'count_include_pad', 'B']
+
+    def extra_repr(self) -> str:
+        return 'kernel_size={}, stride={}, padding={}, B={}'.format(
+            self.kernel_size, self.stride, self.padding, self.B
+        )
+
+
+
+class AvgPool2d(_AvgPoolNd):
+    r"""Applies a 2D average pooling over an input signal composed of several input
+    planes.
+    In the simplest case, the output value of the layer with input size :math:`(N, B, C, H, W)`,
+    output :math:`(N, B, C, H_{out}, W_{out})` and :attr:`kernel_size` :math:`(kH, kW)`
+    can be precisely described as:
+    .. math::
+        out(N_i, B_i, C_j, h, w)  = \frac{1}{kH * kW} \sum_{m=0}^{kH-1} \sum_{n=0}^{kW-1}
+                               input(N_i, C_j, stride[0] \times h + m, stride[1] \times w + n)
+    If :attr:`padding` is non-zero, then the input is implicitly zero-padded on both sides
+    for :attr:`padding` number of points.
+    Note:
+        When ceil_mode=True, sliding windows are allowed to go off-bounds if they start within the left padding
+        or the input. Sliding windows that would start in the right padded region are ignored.
+    The parameters :attr:`kernel_size`, :attr:`stride`, :attr:`padding` can either be:
+        - a single ``int`` -- in which case the same value is used for the height and width dimension
+        - a ``tuple`` of two ints -- in which case, the first `int` is used for the height dimension,
+          and the second `int` for the width dimension
+    Args:
+        kernel_size: the size of the window
+        stride: the stride of the window. Default value is :attr:`kernel_size`
+        padding: implicit zero padding to be added on both sides
+        ceil_mode: when True, will use `ceil` instead of `floor` to compute the output shape
+        count_include_pad: when True, will include the zero-padding in the averaging calculation
+        divisor_override: if specified, it will be used as divisor, otherwise size of the pooling region will be used.
+
+    Shape:
+        - Input: :math:`(N, B, C, H_{in}, W_{in})` or :math:`(C, H_{in}, W_{in})`.
+        - Output: :math:`(N, B, C, H_{out}, W_{out})` or :math:`(C, H_{out}, W_{out})`, where
+          .. math::
+              H_{out} = \left\lfloor\frac{H_{in}  + 2 \times \text{padding}[0] -
+                \text{kernel\_size}[0]}{\text{stride}[0]} + 1\right\rfloor
+          .. math::
+              W_{out} = \left\lfloor\frac{W_{in}  + 2 \times \text{padding}[1] -
+                \text{kernel\_size}[1]}{\text{stride}[1]} + 1\right\rfloor
+    Examples::
+        >>> # pool of square window of size=3, stride=2, B=2
+        >>> m = nn.AvgPool2d(3, stride=2, B=2)
+        >>> # pool of non-square window
+        >>> m = nn.AvgPool2d((3, 2), stride=(2, 1, B=2))
+        >>> input = torch.randn(20, 2, 16, 50, 32)
+        >>> output = m(input)
+    """
+    __constants__ = ['kernel_size', 'stride', 'padding', 'ceil_mode', 'count_include_pad', 'divisor_override', 'B']
+
+    kernel_size: _size_2_t
+    stride: _size_2_t
+    padding: _size_2_t
+    ceil_mode: bool
+    count_include_pad: bool
+
+    def __init__(self, kernel_size: _size_2_t, stride: Optional[_size_2_t] = None, padding: _size_2_t = 0,
+                 ceil_mode: bool = False, count_include_pad: bool = True, divisor_override: Optional[int] = None,
+                 B: Optional[int] = 0) -> None:
+        super(AvgPool2d, self).__init__()
+        self.kernel_size = kernel_size
+        self.stride = stride if (stride is not None) else kernel_size
+        self.padding = padding
+        self.ceil_mode = ceil_mode
+        self.count_include_pad = count_include_pad
+        self.divisor_override = divisor_override
+        self.B = B
+
+    def forward(self, input: Tensor) -> Tensor:
+        N, B, C, H, W = input.size()
+        input = input.view(N * B, C, H, W)
+        out = F.avg_pool2d(input, self.kernel_size, self.stride,
+                            self.padding, self.ceil_mode, self.count_include_pad, self.divisor_override)
+        _, _, H_out, W_out = out.size()
+        return out.view(N, B, C, H_out, W_out)
